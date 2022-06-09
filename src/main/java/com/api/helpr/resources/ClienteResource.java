c6efac7e98cf47b4d99b7ba0c8f321868a3bdc55
AlexandreMercador <amercador@gmail.com>
2022-06-09 16:04:06 -0300

    Criação método delete cliente

--- src/main/java/com/api/helpr/resources/ClienteResource.java
+++ src/main/java/com/api/helpr/resources/ClienteResource.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -55,6 +56,12 @@ public class ClienteResource {
 			@PathVariable Integer id, @RequestBody ClienteDTO objDto){
 		Cliente obj = service.updateCliente(id, objDto);
 		return ResponseEntity.ok().body(new ClienteDTO(obj));
+	}
+	
+	@DeleteMapping(value="{id}")
+	public ResponseEntity<ClienteDTO> deleteCliente(@PathVariable Integer id){
+		service.deleteCliente(id);
+		return ResponseEntity.noContent().build();
 	}
 	
 }
